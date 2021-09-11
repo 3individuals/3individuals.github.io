2ca377fa8eb4a5bc8328ba841bd8c0cad0fa39d2
b_shay <bharadh0001@gmail.com>
2021-09-11 17:51:43 +0400

    added menu.a lot more to do

---
+++ menu.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+document.getElementById("menu_button").addEventListener("click",function(){
+    if (document.querySelector(".menu").style.display=="none"){
+    document.getElementById("menu_button").innerText="close"
+    document.querySelector(".menu").style.display="grid"
+    }else{
+        document.getElementById("menu_button").innerText="menu"
+        document.querySelector(".menu").style.display="none"
+    }
+})
+}
