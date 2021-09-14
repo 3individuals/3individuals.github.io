411c1db789a31463fd9fcb0627dfd836f57d3db7
akash <abv3gamer@gmail.com>
2021-09-14 19:05:01 +0400

    fixed clock again :)

--- clock.js
+++ clock.js
@@ -2,5 +2,10 @@ setInterval(time, 1000);
 
 function time() {
     var time = new Date();
-    document.getElementById('clock').innerText = time.getHours() + ":" + time.getMinutes();
+    if (time.getMinutes()<10) {
+        document.getElementById('clock').innerText = time.getHours() + ":0" + time.getMinutes();
+    }
+    else {
+        document.getElementById('clock').innerText = time.getHours() + ":" + time.getMinutes();
+    }
 }
