57590dd16966c5f74e093daeefe43cdfdd0dc810
Nufais Nisam <nufaisnisam@gmail.com>
2021-09-15 08:06:00 +0400

    changes to the clock again. 😝

--- clock.js
+++ clock.js
@@ -1,11 +1,14 @@
-setInterval(time, 1000);
-
 function time() {
-    var time = new Date();
-    if (time.getMinutes()<10) {
-        document.getElementById('clock').innerText = time.getHours() + ":0" + time.getMinutes();
-    }
-    else {
-        document.getElementById('clock').innerText = time.getHours() + ":" + time.getMinutes();
-    }
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+
+    let time = hh + ":" + mm;
+
+    document.getElementById("clock").innerText = time;
 }
+
+setInterval(time, 1000)
