77d666a9fb0df6791540879388f3c93e62f7a905
b_shay <bharadh0001@gmail.com>
2021-09-15 22:14:00 +0400

    adding buttons and increasing size of dock

--- add_websites.js
+++ add_websites.js
@@ -7,4 +7,12 @@ function add_website_button(){
 }
 function add_website(){
     document.getElementById("add_website_menu").style.display="none"
+    var buttons=document.querySelectorAll("#dock button")
+    for (var i=0;i<buttons.length-1;i+=1){
+        if (buttons[i].style.display=="none"){
+            document.getElementById("dock").style.width=document.getElementById("dock").offsetWidth+60+"px"
+            buttons[i].style.display="block"
+            break
+        }
+    }
 }
