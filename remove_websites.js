2fa3fdb73db170f38ea28f6db10a42f750a44915
b_shay <bharadh0001@gmail.com>
2021-09-19 22:13:24 +0400

    remove done

---
+++ remove_websites.js
@@ -0,0 +1,36 @@
+function remove(){
+    var buttons=document.querySelector("#dock")
+    var remove=document.getElementById("remove")
+    var dock=document.getElementById("dock")
+
+    buttons.oncontextmenu=function(button_click){
+        button_click.preventDefault()
+        if (button_click.currentTarget!=button_click.target && button_click.target.id!="add_button"){
+            if (remove.style.display=="none"){
+                remove.style.display="block"
+                remove.onclick=function(){
+                    button_click.target.style.display="none"
+                    dock.style.width=dock.offsetWidth-60+"px"
+                    remove.style.display="none"
+                    return check_dock()
+                }
+            }else{
+                remove.style.display="none"
+            }
+        }
+    }
+    window.onclick=function(){
+        remove.style.display="none"
+    }
+}
+function check_dock(){
+    var button=document.querySelectorAll("#dock button")
+    for(var i=0;i<button.length-1;i+=1){
+        if (button[i].style.display=="block"){
+            if (i==button.length-2){
+                button[i+1].style.display="block"
+            }
+        }
+    return
+    }
+}
