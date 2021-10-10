b83abe47466f89a06e04e2d6b91925337c9c1bd8
b_shay <bharadh0001@gmail.com>
2021-10-10 19:32:08 +0400

    buttons works properly after refreshing

--- add_buttons.js
+++ add_buttons.js
@@ -1,6 +1,5 @@
 window.onload=function(){
     var box=document.getElementById("box");
-    var buttons=document.querySelectorAll("#dock button");
 
     if (localStorage.getItem("bg")==undefined){
         document.getElementById("main_bg").src="walls/Mr. Lee.jpg";
@@ -9,12 +8,15 @@ window.onload=function(){
         document.getElementById("main_bg").src=localStorage.getItem("bg");
     };
     
-    if (localStorage.length==buttons.length){
+    if (localStorage.length==document.querySelectorAll("#dock button").length){
         document.getElementById("add_button").style.display="none";
     };
     
     for (var i=0;i<localStorage.length;i+=1){
         if (localStorage.key(i)!="bg"){
+
+            var button=document.getElementById(localStorage.key(i))
+
             if (i<5){
                 box.style.width=box.offsetWidth+80+"px"
             };
@@ -22,8 +24,8 @@ window.onload=function(){
                 box.style.height=box.offsetHeight+90+"px"
             }
             box.style.columnGap="10px"
-            document.getElementById(localStorage.key(i)).style.display="block";
-            buttons[i].innerHTML=JSON.parse(localStorage.getItem(localStorage.key(i)))["name"]
+            button.style.display="block";
+            button.innerHTML=JSON.parse(localStorage.getItem(localStorage.key(i)))["name"]
         };
     };
 }
